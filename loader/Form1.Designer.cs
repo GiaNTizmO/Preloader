d5a149971374d2ef44022d9c117cfcb077cf9230
Alikhan <autocardag@gmail.com>
2020-02-04 22:48:23 +0300

    Initial commit
    Initial commit

---
+++ loader/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace loader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.posLable = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::loader.Properties.Resources._212_2125475_dark_blue_material_design;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(611, 334);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(198, 256);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(200, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(270, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // posLable
+            // 
+            this.posLable.AutoSize = true;
+            this.posLable.Location = new System.Drawing.Point(579, 296);
+            this.posLable.Name = "posLable";
+            this.posLable.Size = new System.Drawing.Size(14, 15);
+            this.posLable.TabIndex = 4;
+            this.posLable.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 320);
+            this.Controls.Add(this.posLable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Minecraft Preloader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Label posLable;
+    }
+}
+
---
+++ loader/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loader
+{
+    
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            label2.Parent = pictureBox1;
+            label2.BackColor = Color.Transparent;
+            progressBar.Maximum = 100;
+
+            if (!Directory.Exists("C:\\VexarMC"))
+            {
+                Directory.CreateDirectory("C:\\VexarMC");
+            }
+            
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Java Verify
+        {
+            timer1.Stop();
+
+            var wc = new WebClient();
+            progressBar.Value = 1;
+            var md5 = MD5.Create();
+
+            if (IntPtr.Size == 8)
+            {
+                if (File.Exists("C:\\VexarMC\\java_64.zip"))
+                {
+                    var stream = File.OpenRead("C:\\VexarMC\\java_64.zip");
+                    byte[] checksum = md5.ComputeHash(stream);
+                    string output = BitConverter.ToString(checksum).Replace("-", String.Empty).ToLower();
+                    stream.Close();
+                    Stream streamphp = wc.OpenRead("http://launch.vexarmc.ru/launch.php?starter=64");
+                    StreamReader reader = new StreamReader(streamphp);
+                    String content = reader.ReadToEnd();
+                    Match[] MD5HashesServer = Regex.Matches(content, @"[A-z0-9]{32}")
+                       .Cast<Match>()
+                       .ToArray();
+                    if (MD5HashesServer[1].ToString() == output)
+                    {
+                        //Java on client is valid!
+                        timer3.Start();
+                        progressBar.Value = 75;
+                    }
+                    else
+                    {
+                        timer2.Start();
+
+                    }
+                }
+                else
+                {
+                    timer2.Start();
+                }
+            }
+            else if (IntPtr.Size == 4)
+            {
+                if (File.Exists("C:\\VexarMC\\java_32.zip"))
+                {
+                    var stream = File.OpenRead("C:\\VexarMC\\java_32.zip");
+                    byte[] checksum = md5.ComputeHash(stream);
+                    string output = BitConverter.ToString(checksum).Replace("-", String.Empty).ToLower();
+                    stream.Close();
+                    Stream streamphp = wc.OpenRead("http://launch.vexarmc.ru/launch.php?starter=32");
+                    StreamReader reader = new StreamReader(streamphp);
+                    String content = reader.ReadToEnd();
+                    /* Regex word = new Regex(@";[A-z0-9]{32};U");
+                     MessageBox.Show(word.Match(content).ToString());*/
+                    Match[] MD5HashesServer = Regex.Matches(content, @"[A-z0-9]{32}")
+                       .Cast<Match>()
+                       .ToArray();
+                    if (MD5HashesServer[1].ToString() == output)
+                    {
+                        timer3.Start();
+                        progressBar.Value = 75;
+                    }
+                    else
+                    {
+                        timer2.Start();
+
+                    }
+                }
+                else
+                {
+                    timer2.Start();
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) //Java Downloader
+        {
+            timer2.Stop();
+           
+                var wc1 = new WebClient();
+            wc1.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            wc1.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            if (IntPtr.Size == 8)
+            {
+                // 64 bit OS
+                wc1.DownloadFileAsync(new Uri("http://launch.vexarmc.ru/clients/java_64.zip"), "C:\\VexarMC\\java_64.zip");
+            }
+            else if (IntPtr.Size == 4)
+            {
+                // 32 bit OS
+                wc1.DownloadFileAsync(new Uri("http://launch.vexarmc.ru/clients/java_32.zip"), "C:\\VexarMC\\java_32.zip");
+            }
+            else
+            {
+                MessageBox.Show("Произошла ошибка в работе стартера! Пожалуйста, отправьте администратору скриншот данной ошибки!" + "\nUnable get OsArch, current is:" + Environment.OSVersion.Platform, "Ошибка",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 70;
+                label2.Text = "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+                progressBar.Value = (progressBar.Value/10) + int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+
+        private void timer3_Tick(object sender, EventArgs e) // Verify launcher.jar
+        {
+            timer3.Stop();
+            if (File.Exists("C:\\VexarMC\\launcher.jar"))
+            {
+                var md5 = MD5.Create();
+                var wc = new WebClient();
+                if (IntPtr.Size == 8)
+                {
+                    Stream streamphp = wc.OpenRead("http://launch.vexarmc.ru/launch.php?starter=64");
+                    StreamReader reader = new StreamReader(streamphp);
+                    String content = reader.ReadToEnd();
+                    Match[] MD5HashesServer_launcher = Regex.Matches(content, @"[A-z0-9]{32}")
+                               .Cast<Match>()
+                               .ToArray();
+                    var stream = File.OpenRead("C:\\VexarMC\\launcher.jar");
+                    
+                    byte[] checksum = md5.ComputeHash(stream);
+                    stream.Close();
+                    string output = BitConverter.ToString(checksum).Replace("-", String.Empty).ToLower();
+                    if (MD5HashesServer_launcher[0].ToString() == output)
+                    {
+                        //Launcher on client is valid!
+                        timer5.Start();
+                    }
+                    else
+                    {
+                        timer4.Start();
+                    }
+
+                }
+                if (IntPtr.Size == 4)
+                {
+                    Stream streamphp = wc.OpenRead("http://launch.vexarmc.ru/launch.php?starter=32");
+                    StreamReader reader = new StreamReader(streamphp);
+                    String content = reader.ReadToEnd();
+                    Match[] MD5HashesServer_launcher = Regex.Matches(content, @"[A-z0-9]{32}")
+                               .Cast<Match>()
+                               .ToArray();
+                    var stream = File.OpenRead("C:\\VexarMC\\launcher.jar");
+                    
+                    byte[] checksum = md5.ComputeHash(stream);
+                    stream.Close();
+                    string output = BitConverter.ToString(checksum).Replace("-", String.Empty).ToLower();
+                    
+                    if (MD5HashesServer_launcher[0].ToString() == output)
+                    {
+                        //Launcher on client is valid!
+                        progressBar.Value = progressBar.Value + (progressBar.Value / 100 * 5);
+                        timer5.Start();
+                    }
+                    else
+                    {
+                        timer4.Start();
+                    }
+                }
+
+            }
+            else
+            {
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e) // download launcher.jar
+        {
+            timer4.Stop();
+            var wc = new WebClient();
+            //wc.DownloadFileAsync(new Uri("http://launch.vexarmc.ru/launcher.jar"), "C:\\VexarMC\\launcher.jar");
+            wc.DownloadFile("http://launch.vexarmc.ru/launcher.jar", "C:\\VexarMC\\launcher.jar");
+            progressBar.Value = progressBar.Value + (progressBar.Value / 100 * 5);
+            timer3.Start();
+        }
+
+        private void timer5_Tick(object sender, EventArgs e) //unzip java
+        {
+            timer5.Stop();
+            progressBar.Value = progressBar.Value + 1;
+            if (IntPtr.Size == 8)
+            {
+                    DirectoryInfo directoryinfo = new DirectoryInfo("C:\\VexarMC\\java_64");
+                    if (directoryinfo.Exists) directoryinfo.Delete(true);
+                Task.Factory.StartNew(() =>
+                {
+                    ZipFile.ExtractToDirectory("C:\\VexarMC\\java_64.zip", "C:\\VexarMC\\java_64");
+                });
+
+
+            }
+            if (IntPtr.Size == 4)
+            {
+                ZipFile.ExtractToDirectory("C:\\VexarMC\\java_32.zip", "C:\\VexarMC\\java_32");
+            }
+
+            progressBar.Value = progressBar.Value + (progressBar.Value / 100 * 15);
+            timer6.Start();
+        }
+
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
+            this.BeginInvoke((MethodInvoker)delegate {
+                timer1.Start();
+            });
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            timer6.Stop();
+            int checkCount = Int32.Parse(posLable.Text);
+            checkCount = checkCount + 1;
+            posLable.Text = checkCount.ToString();
+            MessageBox.Show(checkCount.ToString());
+            if (checkCount == 1)
+            {
+                timer1.Start();
+                return;
+            }
+            progressBar.Value = progressBar.Value + (progressBar.Value / 100 * 4);
+            System.Diagnostics.Process.Start("C:\\VexarMC\\java_64\\jre-8-51-x64\\bin\\javaw.exe", "C:\\VexarMC\\launcher.jar");
+        }
+    }
+}
